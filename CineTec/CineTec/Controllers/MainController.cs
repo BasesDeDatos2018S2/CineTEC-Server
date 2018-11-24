61e323334f16cf1edbd7b858a80eeadd24a7f594
Crisptofer Fernández Fernández <alberto12ff@gmail.com>
2018-11-23 23:38:58 -0600

    Base de datos
    Adición de la conexión a la base de datos y manejo de información de esta

---
+++ CineTec/CineTec/Controllers/MainController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Data;
+using System.Web.Http;
+using CineTec.Logic;
+using Npgsql;
+
+namespace CineTec.Controllers
+{
+    public class prueba
+    {
+        public int Id_actor { get; set; }
+        public int Id_movie { get; set; }        
+    }
+
+    public class MainController : ApiController
+    {
+        // GET: api/Main
+        public IHttpActionResult Get()
+        {
+            ConnectionPostgreSQL cn = new ConnectionPostgreSQL();
+            NpgsqlConnection connection = cn.OpenConection();
+            //DataTable dt = new DataTable();
+            List<object> list = new List<object>();
+            try /* Select After Validations*/
+            {
+                using (connection)
+                {
+                
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Select * from Actors_Movie";
+                    cmd.CommandType = CommandType.Text;
+                    //NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        prueba prueba = new prueba();
+                        prueba.Id_actor = Convert.ToInt32(reader["Id_actor"]);
+                        prueba.Id_movie = Convert.ToInt32(reader["Id_movie"]);
+                        list.Add(prueba);
+
+                    }
+                    //da.Fill(dt);
+                    cmd.Dispose();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex) { }
+
+
+
+            return Ok(list[1]);
+        }
+
+        // GET: api/Main/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Main
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Main/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Main/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CineTec/CineTec/Logic/ConnectionPostgreSQL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace CineTec.Logic { 
+    public class ConnectionPostgreSQL
+    {
+
+        public NpgsqlConnection OpenConection()
+        {
+
+            NpgsqlConnection connection = new NpgsqlConnection();
+            var conectionString = "Server=192.168.100.12;Port=5432;User Id=postgres;Password=1517;Database=CineTec;";
+
+            if (!string.IsNullOrWhiteSpace(conectionString))
+            {
+
+                try
+                {
+                    connection = new NpgsqlConnection(conectionString);
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    connection.Close();
+                }
+
+            }
+            return connection;
+        }
+    }
+}
